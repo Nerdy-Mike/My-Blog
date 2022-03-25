21e8d759da86d20dec48b6e7794329b57187de4e
Nerdy Mike <maule.hc@gmail.com>
2022-03-25 16:22:14 +0700

    fixed sticky header

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -12,26 +12,28 @@ import SideBarLeft from './SideBarLeft'
 const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
-      <div className="flex h-full flex-col items-center justify-between ">
-        <header className="sticky top-0 z-50 mx-auto flex w-screen items-center justify-between py-2 px-6 backdrop-blur-lg backdrop-filter md:px-16 ">
-          <Link href="/" aria-label={siteMetadata.headerTitle}>
-            <div className="flex items-center justify-between">
-              <div className="mr-3">
-                <Logo />
-              </div>
-              {typeof siteMetadata.headerTitle === 'string' ? (
-                <div className="hidden h-6 text-2xl font-semibold sm:block">
-                  {siteMetadata.headerTitle}
+      <div className="flex h-full flex-col items-center justify-between  ">
+        <header className="sticky top-0 z-50 mx-auto  flex w-screen justify-center py-4 px-6 backdrop-blur-lg backdrop-filter md:px-16 ">
+          <div className="flex w-full max-w-screen-2xl justify-between ">
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
+              <div className="flex items-center justify-between">
+                <div className="mr-3">
+                  <Logo />
                 </div>
-              ) : (
-                siteMetadata.headerTitle
-              )}
+                {typeof siteMetadata.headerTitle === 'string' ? (
+                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                    {siteMetadata.headerTitle}
+                  </div>
+                ) : (
+                  siteMetadata.headerTitle
+                )}
+              </div>
+            </Link>
+            <div className="flex items-center text-base leading-5">
+              {/* <SearchBar posts={[]} /> */}
+              <ThemeSwitch />
+              <MobileNav />
             </div>
-          </Link>
-          <div className="flex items-center text-base leading-5">
-            {/* <SearchBar posts={[]} /> */}
-            <ThemeSwitch />
-            <MobileNav />
           </div>
         </header>
         <div className="grid max-w-screen-2xl grid-cols-10 px-2 md:px-16">
